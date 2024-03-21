07caf62a666b32083b995cc317b9be98c89828bd
happygorilla <163237534+happygorilla@users.noreply.github.com>
2024-03-21 19:22:01 +0900

    Add files via upload

---
+++ UsedCar.h
@@ -0,0 +1,40 @@
+#ifndef USEDCAR_H
+#define USEDCAR_H
+
+#include <string>
+using namespace std;
+
+class UsedCar {
+private:
+    string make;
+    string model;
+    int year;
+    double price;
+    int mileage;
+
+public:
+    // 생성자
+    UsedCar(string _make, string _model, int _year, double _price, int _mileage);
+
+    // 소멸자
+    ~UsedCar();
+
+    // 접근자 (getter)
+    string getMake();
+    string getModel();
+    int getYear();
+    double getPrice();
+    int getMileage();
+
+    // 설정자 (setter)
+    void setYear(int _year);
+    void setMileage(int _mileage);
+
+    // 주행거리 및 연식에 따른 할인 계산
+    double calculateDiscountedPrice();
+
+    // 차량 정보 출력
+    void displayInfo();
+};
+
+#endif
